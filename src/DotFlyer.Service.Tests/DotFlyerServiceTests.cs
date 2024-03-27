aca8d2ded7e6daccf3bdf653b051cb887617694d
Artsiom Patskou <apatskou@uniphar.ie>
2024-03-27 10:40:56 +0000

    Added a partial integration test

---
+++ src/DotFlyer.Service.Tests/DotFlyerServiceTests.cs
@@ -0,0 +1,80 @@
+﻿using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+using Azure.Security.KeyVault.Secrets;
+using DotFlyer.Shared.Payload;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+using System.Text.Json;
+
+namespace DotFlyer.Service.Tests;
+
+[TestClass, TestCategory("Integration")]
+public class DotFlyerServiceTests
+{
+    private static ServiceBusClient? _serviceBusClient;
+    private static ServiceBusSender? _serviceBusSender;
+    private static HttpClient? _httpClient;
+
+    private static string? _senderEmail;
+    private static string? _receiverEmail;
+
+    [ClassInitialize]
+    public static async Task ClassInitialize(TestContext context)
+    {
+        Environment.SetEnvironmentVariable("AZURE_KEY_VAULT_NAME", "uni-devops-app-dev-kv");
+
+        var azureKeyVaultName = Environment.GetEnvironmentVariable("AZURE_KEY_VAULT_NAME");
+
+        SecretClient secretClient = new(new($"https://{azureKeyVaultName}.vault.azure.net/"), new DefaultAzureCredential());
+
+        var serviceBusNamespaceName = await secretClient.GetSecretAsync("AzureServiceBus--Name");
+
+        _serviceBusClient = new(serviceBusNamespaceName.Value.Value, new DefaultAzureCredential());
+
+        _serviceBusSender = _serviceBusClient.CreateSender("dotflyer");
+
+        var sendgridApiKeyIntegrationTest = await secretClient.GetSecretAsync("SendGrid--ApiKeyIntegrationTest");
+
+        _httpClient = new() { BaseAddress = new("https://api.sendgrid.com/v3/") };
+        _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", $"Bearer {sendgridApiKeyIntegrationTest.Value.Value}");
+
+        _senderEmail = (await secretClient.GetSecretAsync("integration-test-dotflyer-sender")).Value.Value;
+        _receiverEmail = (await secretClient.GetSecretAsync("integration-test-dotflyer-receiver")).Value.Value;
+    }
+
+
+    [TestMethod]
+    public async Task DotFlyerService_ShouldSendEmail()
+    {
+        var randomGuid = Guid.NewGuid();
+
+        EmailMessage emailMessage = new()
+        {
+            Subject = $"Test email {randomGuid}",
+            Body = $"Test email body {randomGuid}",
+            FromEmail = _senderEmail!,
+            FromName = "Integration Test",
+            To = [
+                new()
+                {
+                    Email = _receiverEmail!,
+                    Name = "Integration Test Destination"
+                }
+            ]
+        };
+
+        var message = new ServiceBusMessage(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(emailMessage)));
+
+        await _serviceBusSender!.SendMessageAsync(message);
+
+        // TODO: Verify email is sent using ADX
+    }
+
+    [ClassCleanup]
+    public static async Task ClassCleanup()
+    {
+        await _serviceBusSender!.DisposeAsync();
+        await _serviceBusClient!.DisposeAsync();
+        _httpClient!.Dispose();
+    }
+}
