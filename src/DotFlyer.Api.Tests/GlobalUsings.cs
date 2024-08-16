fd1b253c857a24ae5762e67d4afb3660bd804353
Artsiom Patskou <apatskou@uniphar.ie>
2024-08-16 11:57:36 +0100

    Added SMS endpoint integration tests

---
+++ src/DotFlyer.Api.Tests/GlobalUsings.cs
@@ -0,0 +1,15 @@
+﻿global using Azure.Identity;
+global using Azure.Security.KeyVault.Secrets;
+global using DotFlyer.Common.Payload;
+global using DotFlyer.Service.AzureDataExplorer.Models;
+global using DotFlyer.Service.AzureDataExplorer.Tables;
+global using FluentAssertions;
+global using Kusto.Cloud.Platform.Data;
+global using Kusto.Data;
+global using Kusto.Data.Common;
+global using Kusto.Data.Net.Client;
+global using Microsoft.Identity.Client;
+global using Polly;
+global using System.Net;
+global using System.Text;
+global using System.Text.Json;
