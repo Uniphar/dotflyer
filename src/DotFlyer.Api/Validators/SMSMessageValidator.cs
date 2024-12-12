d62fbb02ebdde5b9ddd37b81723ca6da1ffb077c
Artsiom Patskou <apatskou@uniphar.ie>
2024-12-12 10:28:17 +0000

    Refactored SMS message validator

--- src/DotFlyer.Api/Validators/SMSMessageValidator.cs
+++ src/DotFlyer.Api/Validators/SMSMessageValidator.cs
@@ -4,15 +4,19 @@ public class SMSMessageValidator : AbstractValidator<SMSMessage>
 {
     public SMSMessageValidator()
     {
-        RuleFor(x => x.To)
-            .NotEmpty().WithMessage("'To' field is required")
-            .CustomAsync(async (to, context, cancellationToken) =>
+        RuleFor(x => x.To).CustomAsync(async (to, context, cancellationToken) =>
+        {
+            if (string.IsNullOrWhiteSpace(to))
             {
-                if ((await PhoneNumberResource.FetchAsync(to)).Valid is false)
-                {
-                    context.AddFailure("'To' field should be a valid phone number in E.164 format");
-                }
-            });
+                context.AddFailure("'To' field is required");
+                return;
+            }
+
+            if ((await PhoneNumberResource.FetchAsync(to)).Valid is false)
+            {
+                context.AddFailure("'To' field should be a valid phone number in E.164 format");
+            }
+        });
 
 
         RuleFor(x => x.Body).NotEmpty().WithMessage("'Body' field is required");
