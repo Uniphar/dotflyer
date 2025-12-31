107e644979296c0b22322f7c832aae699e67f6a8
Borys Generalov <bgeneralov@uniphar.ie>
2025-12-31 13:10:07 +0000

    fix secret rotation model (tenant id and app id are optional); updated email html component

--- src/DotFlyer.Common/EmailTemplates/ManualSecretRotationModel.cs
+++ src/DotFlyer.Common/EmailTemplates/ManualSecretRotationModel.cs
@@ -4,10 +4,10 @@ namespace DotFlyer.Common.EmailTemplates;
 
 public class ManualSecretRotationModel
 {
-    public required string TenantId { get; init; }
-    public required string AppId { get; init; }
+    public string? TenantId { get; init; }
+    public string? AppId { get; init; }
     public required string ResourceName { get; init; }
-    public IEnumerable<string>? KeyVaults { get; set; }
+    public required IEnumerable<string> KeyVaults { get; init; }
     public required string SecretName { get; init; }
     public DateTime OldSecretDeletionDateUtc { get; set; }
     public string? PwPushUrl { get; set; }
