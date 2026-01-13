b67b28082e48c61f21d4f740ebc1267546f00b4e
Borys Generalov <bgeneralov@uniphar.ie>
2026-01-13 10:12:46 +0000

    Handle invalid email template as warning, not error (#29)
    * treat the missing or invalid template as warning, not error

---
+++ src/DotFlyer.EmailTemplates.Tests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using DotFlyer.Common.EmailTemplates;
+global using DotFlyer.Common.Payload;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Playwright;
+global using System.Text.Json;
